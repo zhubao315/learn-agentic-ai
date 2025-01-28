88bf6a8891ebccd025778c10dbbbbc2be173ce5b
Zia Khan <ziaukhan@hotmail.com>
2025-01-28 14:08:48 +0500

    major update

--- 06_crew_ai/17_colab_poemflow/readme.md
+++
@@ -1,5 +0,0 @@
-# Run Flows in Google Colab
-
-https://colab.research.google.com/drive/1RRsBLOh-3BgshTBJrSfdMChdV8SX0vFI?usp=sharing
-
- 
--- 06_crew_ai/PROJECTS/04_email_auto_responder_flow/src/email_auto_responder_flow/types.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Email(BaseModel):
-    id: str
-    threadId: str
-    snippet: str
-    sender: str
