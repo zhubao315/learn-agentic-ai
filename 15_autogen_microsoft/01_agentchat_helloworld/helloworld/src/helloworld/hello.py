6ef8af03fc72808c211a6aa44259073fed9767eb
Zia Khan <ziaukhan@hotmail.com>
2025-02-18 16:55:58 +0500

    hello world autogen

---
+++ 15_autogen_microsoft/01_agentchat_helloworld/helloworld/src/helloworld/hello.py
@@ -0,0 +1,19 @@
+import os
+import asyncio
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.base import TaskResult
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+async def main() -> None:
+    model_client: OpenAIChatCompletionClient = OpenAIChatCompletionClient(model="gemini-2.0-flash", api_key="gemini-api-key")
+    agent: AssistantAgent = AssistantAgent("assistant", model_client)
+    result: TaskResult = await agent.run(task="What is the capital of France?")
+    for message in result.messages:
+        print(f"{message.source}: {message.content}")
+
+def run_main():
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    run_main()
---
+++ 15_autogen_microsoft/01_agentchat_helloworld/readme.md
@@ -0,0 +1,14 @@
+# AutoGen AgentChat Hello World
+
+https://github.com/microsoft/autogen
+
+https://microsoft.github.io/autogen/stable/user-guide/agentchat-user-guide/tutorial/index.html
+
+https://microsoft.github.io/autogen/stable/user-guide/agentchat-user-guide/tutorial/models.html
+
+
+    uv init --package helloworld
+
+    cd helloworld 
+
+    uv add "autogen-agentchat" "autogen-ext[openai]"
