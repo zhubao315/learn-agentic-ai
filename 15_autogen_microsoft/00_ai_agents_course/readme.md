2d44fa743746cca66a40136517fe2908ca912bd0
Zia Khan <ziaukhan@hotmail.com>
2025-02-19 20:36:31 +0500

    update autogen

--- 15_autogen_microsoft/00_ai_agents_course/readme.md
+++ 15_autogen_microsoft/00_ai_agents_course/readme.md
@@ -3,3 +3,5 @@
 [AI Agents for Beginners - A Course from Microsoft](https://github.com/microsoft/ai-agents-for-beginners)
 
 [Microsoft shared 300 use-cases of GenAI Agents are improving businesses](https://blogs.microsoft.com/blog/2025/02/05/https-blogs-microsoft-com-blog-2024-11-12-how-real-world-businesses-are-transforming-with-ai/)
+
+[Agentic AI in Banking](https://www.linkedin.com/posts/rakeshgohel01_ai-agent-adoption-in-banking-is-taking-a-activity-7293637760362364928-GYl0/)
--- 15_autogen_microsoft/03_tool_calling/toolcalling/src/toolcalling/hello_tool.py
+++ 15_autogen_microsoft/03_tool_calling/toolcalling/src/toolcalling/hello_tool.py
@@ -1,18 +1,41 @@
 import asyncio
 from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.base import TaskResult
+from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
+# Define a model client. You can use other model client that implements
+# the `ChatCompletionClient` interface.
+model_client = OpenAIChatCompletionClient(
+    model="gemini-2.0-flash",
+    api_key="gemini api key",
+)
+
+
+# Define a simple function tool that the agent can use.
+# For this example, we use a fake weather tool for demonstration purposes.
+async def get_weather(city: str) -> str:
+    """Get the weather for a given city."""
+    return f"The weather in {city} is 73 degrees and Sunny."
+
+
+# Define an AssistantAgent with the model, tool, system message, and reflection enabled.
+# The system message instructs the agent via natural language.
+agent = AssistantAgent(
+    name="weather_agent",
+    model_client=model_client,
+    tools=[get_weather],
+    system_message="You are a helpful assistant.",
+    reflect_on_tool_use=True,
+    model_client_stream=True,  # Enable streaming tokens from the model client.
+)
+
+
+# Run the agent and stream the messages to the console.
 async def main() -> None:
-    model_client: OpenAIChatCompletionClient = OpenAIChatCompletionClient(model="gemini-2.0-flash", api_key="gemini-api-key")
-    agent: AssistantAgent = AssistantAgent("assistant", model_client)
-    result: TaskResult = await agent.run(task="What is the capital of France?")
-    for message in result.messages:
-        print(f"{message.source}: {message.content}")
+    await Console(agent.run_stream(task="What is the weather in New York?"))
 
 def run_main():
     asyncio.run(main())
-
 
 if __name__ == "__main__":
     run_main()
