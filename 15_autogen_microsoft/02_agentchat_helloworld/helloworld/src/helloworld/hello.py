ebe4b59eda3c78e9a185cfe122d3692d986080dc
Zia Khan <ziaukhan@hotmail.com>
2025-02-20 11:35:52 +0500

    update autogen helloworld

--- 15_autogen_microsoft/02_agentchat_helloworld/helloworld/src/helloworld/hello.py
+++ 15_autogen_microsoft/02_agentchat_helloworld/helloworld/src/helloworld/hello.py
@@ -1,10 +1,25 @@
 import asyncio
+import os
+from dotenv import load_dotenv
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.base import TaskResult
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
+# Load the environment variables from the .env file
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
 async def main() -> None:
-    model_client: OpenAIChatCompletionClient = OpenAIChatCompletionClient(model="gemini-2.0-flash", api_key="gemini-api-key")
+
+    
+
+    # Create the model client
+    model_client: OpenAIChatCompletionClient = OpenAIChatCompletionClient(model="gemini-2.0-flash", api_key=gemini_api_key)
     agent: AssistantAgent = AssistantAgent("assistant", model_client)
     result: TaskResult = await agent.run(task="What is the capital of France?")
     for message in result.messages:
