c08db1a285c25b6c06025c2dbf6252ab5fd08feb
Sir Qasim <m.qasim077@gmail.com>
2025-04-15 05:48:57 +0500

    DACA micro-services run with .sh or .bat file port updated

--- 01_openai_agents/17_daca_local_dev/03_microservices_with_fastapi/fastapi-daca-tutorial/run_services.sh
+++ 01_openai_agents/17_daca_local_dev/03_microservices_with_fastapi/fastapi-daca-tutorial/run_services.sh
@@ -3,13 +3,13 @@
 # Navigate to the agent_memory_service directory and start the service
 (
   cd agent_memory_service || exit
-  uv run fastapi dev
+  uv run fastapi dev --port 8001
 ) &
 
 # Navigate to the chat-service directory and start the service
 (
   cd chat-service || exit
-  uv run fastapi dev
+  uv run fastapi dev --port 8000
 ) &
 
 # Wait for both background processes to finish
